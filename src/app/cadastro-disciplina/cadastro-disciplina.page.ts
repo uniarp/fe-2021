b28099c47be34a3c13dbfa3ff509c20d155ca529
Adriel <adriel.medeiros7@gmail.com>
2021-03-12 21:07:38 -0300

    criacao das paginas

---
+++ src/app/cadastro-disciplina/cadastro-disciplina.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-disciplina',
+  templateUrl: './cadastro-disciplina.page.html',
+  styleUrls: ['./cadastro-disciplina.page.scss'],
+})
+export class CadastroDisciplinaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/cadastro-software/cadastro-software.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-software',
+  templateUrl: './cadastro-software.page.html',
+  styleUrls: ['./cadastro-software.page.scss'],
+})
+export class CadastroSoftwarePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
