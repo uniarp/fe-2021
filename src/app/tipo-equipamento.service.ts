b51d5dc4f5cc41682686c108264df12941be7cd0
Ramon Meireles da Silva <ramon.meireles32@gmail.com>
2021-03-15 21:20:42 -0300

    TipoEquipamento: Adição dos métodos listar, cadastrar, alterar e excluir

--- src/app/tipo-equipamento.service.ts
+++ src/app/tipo-equipamento.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { TipoEquipamento } from './tipo-equipamento';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TipoEquipamentoService {
 
-  constructor() { }
+  constructor() {
+    console.log("serviço iniciado");
+  }
+
+  alterar(): boolean {
+    return null;
+  } 
+
+  gravar(): boolean {
+    return null;
+  }
+
+  listar(): TipoEquipamento[] {
+    return null;
+  }
+
+  excluir(): boolean {
+    return null;
+  }
 }
