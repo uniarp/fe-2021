fdc9a67b1cc3bfd2e4728017d8d7a26381c5915c
Alexandre Tesser <alexandre.tesser@gmail.com>
2021-03-27 17:00:49 -0300

    cadastro-material.page.ts
    Criado pagina Cadastro Material

--- src/app/cadastro-material/cadastro-material.page.ts
+++ src/app/cadastro-material/cadastro-material.page.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Material } from '../material/material';
+import { MaterialService } from '../material.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-cadastro-material',
+  selector: 'app-cadastro-cadastro-material',
   templateUrl: './cadastro-material.page.html',
   styleUrls: ['./cadastro-material.page.scss'],
 })
-export class CadastroMaterialPage implements OnInit {
+export class CadastroMaterialPage {
 
-  constructor() { }
+  material: Material;
 
-  ngOnInit() {
+  constructor(public materialService:MaterialService,
+    public routerService:Router) {
   }
 
+  ionViewDidEnter(){
+    console.log('cadastro Material page - iondidviewENTER');
+    //instanciando  objeto da classe que vou cadastrari
+    this.material = new Material();
+  }
+
+  gravar(){
+    console.log('Cadastro Material - gravar ');
+    //passar a equipamento que esta sendo cadastrada
+    this.materialService.gravar(this.material);
+    this.routerService.navigate(['lista-material']);
+  }
+
+  cancelar(){
+    console.log('CadastrarMaterial - cancelar');
+    this.material.nome = null;
+    this.material.quantidade = null;
+    this.material.id = null;
+    this.material.descricao = null;
+    this.material.marca = null;
+  }
+
+  listar(){
+    console.log('Cadastro Material - listar');
+    this.routerService.navigate(['lista-material']);
+  }
+
+  novo() {
+    this.routerService.navigateByUrl('/cadastro-material')
+  }
 }
