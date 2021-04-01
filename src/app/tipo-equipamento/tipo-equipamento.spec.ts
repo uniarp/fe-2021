be39d9d98b63d387d06f5a0b7f3442f4d37e92a2
Ramon Meireles da Silva <ramon.meireles32@gmail.com>
2021-03-31 21:45:17 -0300

    TipoEquipamento/TipoEquipamentoService: Remoção da raiz src/app

--- src/app/tipo-equipamento/tipo-equipamento.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TipoEquipamento } from './tipo-equipamento';
-
-describe('TipoEquipamento', () => {
-  it('should create an instance', () => {
-    expect(new TipoEquipamento()).toBeTruthy();
-  });
-});
--- src/app/tipo-equipamento/tipo-equipamento.ts
+++
@@ -1,9 +0,0 @@
-export class TipoEquipamento {
-    
-    id : Number;
-    nome : String;
-    dataAquisicao : Date;
-    marca : String;
-    modelo : String;
-
-}
