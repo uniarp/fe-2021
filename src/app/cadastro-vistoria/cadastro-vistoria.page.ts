b0df5a453b96e504b274c80f7a60c5ae608b0e66
Ramon Meireles da Silva <ramon.meireles32@gmail.com>
2021-03-31 21:50:58 -0300

    Vistoria : Adição das páginas de lista e cadastro

--- src/app/cadastro-vistoria/cadastro-vistoria.page.ts
+++ src/app/cadastro-vistoria/cadastro-vistoria.page.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Vistoria } from '../classes/vistoria';
+import { VistoriaService } from '../services/vistoria.service'
 
 @Component({
   selector: 'app-cadastro-vistoria',
   templateUrl: './cadastro-vistoria.page.html',
   styleUrls: ['./cadastro-vistoria.page.scss'],
 })
-export class CadastroVistoriaPage implements OnInit {
+export class CadastroVistoriaPage {
 
-  constructor() { }
+  vistoria : Vistoria;
 
-  ngOnInit() {
+  constructor(
+    public vistoriaService : VistoriaService,
+    public routerService : Router
+    ) { }
+
+  ionViewDidEnter() {
+    console.log('cadastro vistoria page - iondidviewENTER')
+    this.vistoria = new Vistoria();
   }
 
+  gravar() {
+    console.log('Vistoria - gravar');
+    // passar a vistoria que está sendo cadastrado
+    this.vistoriaService.cadastrar(this.vistoria);
+    this.routerService.navigate(['lista-vistorias']);
+  }
+
+  cancelar() {
+    console.log('Vistoria - cancelar');
+    this.vistoria.id = null;
+    this.vistoria.emConformidade = null;
+    this.vistoria.data = null;
+    this.vistoria.descricao = null;
+    this.vistoria.periodo = null;
+  }
+
+  listar() {
+    console.log('Vistoria - listar');
+    this.routerService.navigate(['lista-vistorias']);
+  }
+
+  novo() {
+    this.routerService.navigateByUrl('/cadastro-vistoria');
+  }
 }
