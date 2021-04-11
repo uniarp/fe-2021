72ecc70613222a688557f31985e8a835a464a9cd
Saul Assenor <saulmartioltg@gmail.com>
2021-04-11 16:56:47 -0300

    alterando retorno lista-material

--- src/app/lista-material/lista-material.page.ts
+++ src/app/lista-material/lista-material.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MaterialService } from '../material.service';
 
 @Component({
   selector: 'app-lista-material',
   templateUrl: './lista-material.page.html',
   styleUrls: ['./lista-material.page.scss'],
 })
-export class ListaMaterialPage implements OnInit {
+export class ListaMaterialPage{
 
-  constructor() { }
+  material : any;
 
-  ngOnInit() {
+  constructor(
+    public materialService:MaterialService,
+    public routerService:Router
+    ) { }
+
+  novo() {
+    this.routerService.navigateByUrl('/cadastro-material');
+  }
+
+  ionViewWillEnter() {
+    this.materialService.listar().subscribe(dados => {
+      this.material = dados;
+      console.log(this.material);
+    });
   }
 
 }
