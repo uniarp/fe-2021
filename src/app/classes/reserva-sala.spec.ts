33872df937209d0e208c144df0b113f3f48e337b
Ramon Meireles da Silva <ramon.meireles32@gmail.com>
2021-04-21 10:44:39 -0300

    Revert "ReservaSala: Geração da Classe"
    This reverts commit dd361b3a0c48732fb765fc476dbe1903263cfb54.

--- src/app/classes/reserva-sala.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ReservaSala } from './reserva-sala';
-
-describe('ReservaSala', () => {
-  it('should create an instance', () => {
-    expect(new ReservaSala()).toBeTruthy();
-  });
-});
--- src/app/classes/reserva-sala.ts
+++
@@ -1,9 +0,0 @@
-export class ReservaSala {
-
-    id? : Number;
-    data? : Date;
-    qtdAlunos? : Number;
-    status? : String;
-    periodo? : String;
-
-}
