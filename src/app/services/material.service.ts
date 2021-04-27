e8110a0f36efb42821cca2f1a2f6a7747d25938b
Ramon Meireles da Silva <ramon.meireles32@gmail.com>
2021-04-26 21:49:48 -0300

    MaterialService : material.service -> services/material.service

--- src/app/services/material.service.ts
+++ src/app/services/material.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Material} from './material/material';
+import { Material } from '../classes/material';
 
 
 @Injectable({
