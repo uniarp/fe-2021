4f60062f11262d4b9dc0f8fa4d887eb0280c3a0b
Ramon Meireles da Silva <ramon.meireles32@gmail.com>
2021-05-02 17:09:46 -0300

    Chave: Criação do service

---
+++ src/app/services/chave.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Chave } from '../classes/chave';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChaveService {
+
+  url = 'https://apoio-uniarp.herokuapp.com/chaves/';
+
+  constructor(public http : HttpClient) {}
+
+  /* Em testes, sujeito a modificações
+  */
+  cadastrar(chave : Chave) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.url + 'cadastrar', chave).subscribe(response => {
+        resolve(response);
+      })
+    });
+  }
+
+  alterar(id : Number, chave : Chave) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.url + id + '/alterar', chave);
+    });
+  }
+
+  listar() {
+    return this.http.get(this.url);
+  }
+
+  excluir(id : Number) {
+    return new Promise((resolve, reject) => {
+      this.http.get(this.url + id + '/excluir');
+    });
+  }
+  
+}
