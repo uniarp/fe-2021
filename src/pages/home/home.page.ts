628b617dfc4c9a0600c28076af4c243a4ddcd579
Ramonms98 <ramon.meireles32@gmail.com>
2021-06-06 16:01:48 -0300

    Home: Adição de reservas de sala e equipamento, Skeleton dos itens recentes

--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ReservaEquipamentoService } from 'src/services/reserva-equipamento.service';
+import { ReservaSalaService } from 'src/services/reserva-sala.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  reservasSala : any;
+  reservasEquipamento : any;
+  itensRecentes : any;
 
+  constructor(public reservaSalaService : ReservaSalaService, public reservaEquipamentoService : ReservaEquipamentoService, public routerService : Router) {}
+
+  ionViewWillEnter() {
+    this.reservasSala = [];
+    this.reservaSalaService.listar().subscribe(dados => {
+      this.reservasSala = dados;
+    });
+    this.reservasEquipamento = [];
+    this.reservaEquipamentoService.listar().subscribe(dados => {
+      this.reservasEquipamento = dados;
+    });
+  }
+
+  listarReservaSala() {
+    this.routerService.navigate(['lista-reservas-sala']);
+  }
+
+  cadastrarReservaSala() {
+    this.routerService.navigate(['cadastro-reserva-sala']);
+  }
+
+  listarReservaEquipamento() {
+    this.routerService.navigate(['lista-reservas-equipamento']);
+  }
+
+  cadastrarReservaEquipamento() {
+    this.routerService.navigate(['cadastro-reserva-equipamento']);
+  }
 }
