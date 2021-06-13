0cf97da1cb8b2fee2cb16166dea55f30cb73763b
Bruno_Pasini <brunopasini@hotmail.com.br>
2021-06-13 19:36:18 -0300

    Botões de ações na tela lista reserva equipamento

--- src/pages/lista-reservas-equipamento/lista-reservas-equipamento.page.ts
+++ src/pages/lista-reservas-equipamento/lista-reservas-equipamento.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ReservaEquipamentoService } from '../../services/reserva-equipamento.service';
+import { AlertController } from '@ionic/angular';
+import { EntregaService } from 'src/services/entrega.service';
 
 @Component({
   selector: 'app-lista-reservas-equipamentos',
@@ -10,10 +12,13 @@ import { ReservaEquipamentoService } from '../../services/reserva-equipamento.se
 export class ListaReservasEquipamentoPage {
   pesquisa = '';
   reservasEquipamento : any;
+  alert: any;
 
   constructor(
     public reservaEquipamentoService:ReservaEquipamentoService,
-    public routerService:Router
+    public alertController: AlertController,
+    public routerService:Router,
+    public entregaService:EntregaService
   ) { }
 
   novo(){
@@ -26,4 +31,89 @@ export class ListaReservasEquipamentoPage {
       this.reservasEquipamento = dados;
     });
   }
+  async presentAlertMultipleButtons() {
+    const alert = await this.alertController.create({
+      header: 'Aviso',
+      subHeader: 'Cancelamento de Reserva',
+      message: 'Tem certeza que deseja cancelar a reserva?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            // rota backend
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Confirmar',
+          handler: () => {
+            // rota backend
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+
+    });
+
+    await alert.present();
+  }
+  entregaEquipamento(id_equipamento:number) {
+    this.alertEntregarDevolver("Você está confirmando uma entrega de equipamento, deseja continuar?","Entrega de equipamento").then((resposta)=>{
+      if(resposta==true){
+        let data = new Date().toLocaleString()
+        const entrega={
+          "entregaEquipamento":id_equipamento,
+          "status":"entregue"
+        }
+        console.log("🚀 ~ ~ entrega", entrega)
+        this.reservaEquipamentoService.cadastrar(entrega).then(()=>{
+          this.reservaEquipamentoService.listar().subscribe(dados => {
+            this.reservasEquipamento = dados;
+          });
+        })
+      }
+    })
+  }
+  devolverEquipamento(id_equipamento:number):void{
+    this.alertEntregarDevolver("Você está confirmando uma devolução de equipamento, deseja continuar?","Devolução de equipamento").then((resposta)=>{
+      if(resposta==true){
+        let data = new Date().toLocaleString();
+        const dados={
+          "status":"devolver",
+        }
+        this.reservaEquipamentoService.alterar(id_equipamento,dados).then(()=>{
+          this.reservaEquipamentoService.listar().subscribe(dados => {
+            this.reservasEquipamento = dados;
+          });
+        })
+      }
+    })
+  }
+
+  async alertEntregarDevolver(message:string,header:string): Promise<boolean> {
+    let resultado: (confirm: boolean) => void;
+    const promise = new Promise<boolean>(resolve => {
+      resultado = resolve;
+    });
+    const alert = await this.alertController.create({
+      header,
+      message,
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role:'cancel',
+          handler: () => resultado(false)
+        },
+        {
+          text: 'Continuar',
+          handler: () => resultado(true)
+        }
+      ]
+    });
+    await alert.present();
+    return promise;
+  }
 }
+
