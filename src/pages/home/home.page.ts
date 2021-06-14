680a2e601f9123276c009f4b58ab46ca251781cd
Ramonms98 <ramon.meireles32@gmail.com>
2021-06-14 12:55:04 -0300

    HomePage: Limpeza de classe.

--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { ReservaEquipamentoService } from 'src/services/reserva-equipamento.service';
 import { ReservaSalaService } from 'src/services/reserva-sala.service';
 
@@ -8,38 +8,14 @@ import { ReservaSalaService } from 'src/services/reserva-sala.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
-  reservasSala : any;
-  reservasEquipamento : any;
-  itensRecentes : any;
-
-  constructor(public reservaSalaService : ReservaSalaService, public reservaEquipamentoService : ReservaEquipamentoService, public routerService : Router) {}
+  constructor(public routerService: Router) { }
 
   ionViewWillEnter() {
-    this.reservasSala = [];
-    this.reservaSalaService.listar().subscribe(dados => {
-      this.reservasSala = dados;
-    });
-    this.reservasEquipamento = [];
-    this.reservaEquipamentoService.listar().subscribe(dados => {
-      this.reservasEquipamento = dados;
-    });
   }
-
-  listarReservasSala() {
-    this.routerService.navigate(['lista-reservas-sala']);
-  }
-
-  cadastrarReservaSala() {
-    this.routerService.navigate(['cadastro-reserva-sala']);
-  }
-
-  listarReservasEquipamento() {
-    this.routerService.navigate(['lista-reservas-equipamento']);
-  }
-
-  cadastrarReservaEquipamento() {
-    this.routerService.navigate(['cadastro-reserva-equipamento']);
-  }
+ 
 }
+
