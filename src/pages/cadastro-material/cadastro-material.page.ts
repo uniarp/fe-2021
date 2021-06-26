b0450c73862f76eb8161b6bbba317608d38b9567
Ramonms98 <ramon.meireles32@gmail.com>
2021-06-26 14:57:48 -0300

    Repara conflito de merge

--- src/pages/cadastro-material/cadastro-material.page.ts
+++ src/pages/cadastro-material/cadastro-material.page.ts
@@ -12,42 +12,28 @@ export class CadastroMaterialPage {
 
   material: Material;
 
-  constructor(public materialService:MaterialService,
-    public routerService:Router) {
+  constructor(public materialService: MaterialService,
+    public routerService: Router) {
   }
 
-  ionViewDidEnter(){
+  ionViewDidEnter() {
     console.log('cadastro Material page - iondidviewENTER');
     //instanciando  objeto da classe que vou cadastrari
     this.material = new Material();
   }
 
-  cadastrar(){
-    console.log('Cadastro Material - cadastrar ');
-    //passar a equipamento que esta sendo cadastrada
-<<<<<<< HEAD
-<<<<<<< HEAD
-    this.materialService.cadastrar(this.material);
-    this.routerService.navigate(['lista-materiais']);
-=======
-    this.materialService.cadastrar(this.material).then(()=>{
-    this.routerService.navigate(['lista-material']);
-    })
->>>>>>> origin
-=======
-    this.materialService.cadastrar(this.material).then(()=>{
-    this.routerService.navigate(['lista-material']);
-    })
->>>>>>> fa6f5ac8ee0fc7abb3aa37042863d91edeece28b
+  cadastrar() {
+    this.materialService.cadastrar(this.material).then(() => {
+      this.routerService.navigate(['lista-materiais']);
+    });
   }
 
-  cancelar(){
+  cancelar() {
     console.log('CadastrarMaterial - cancelar');
     this.routerService.navigateByUrl('/home')
   }
 
-  listar(){
-    console.log('Cadastro Material - listar');
+  listar() {
     this.routerService.navigate(['lista-material']);
   }
 
