bd1f6501e46f2b5686271ffe421a1cc9be98c2d8
Leonardo <leokondratme@gmail.com>
2021-06-28 13:31:06 -0300

    ADICAO DE POPUP PARA CANCELAMENTO DE RESERVA NA LISTA RESERVA EQUIPAMENTO E LISTA RESERVA SALA

--- src/services/software.service.ts
+++ src/services/software.service.ts
@@ -8,6 +8,7 @@ import { Injectable } from '@angular/core';
 })
 export class SoftwareService {
   url = 'https://apoio-uniarp.herokuapp.com/softwares/';
+  
 
   constructor(public http : HttpClient,) {
 
@@ -30,7 +31,8 @@ export class SoftwareService {
     return new Promise((resolve, reject) => {
       this.http.get(this.url + id + '/excluir');
     });
-  }
+  } 
+
   alterar(id: Number, software: Software) {
     return new Promise((resolve, reject) => {
       this.http.post(this.url + id + '/alterar', software);
