df922eba102ecb90756bf155b02a493bd0c5f22b
Ramonms98 <ramon.meireles32@gmail.com>
2021-06-28 21:28:30 -0300

    Mudança na classe de sala

--- src/model/sala.ts
+++ src/model/sala.ts
@@ -4,8 +4,10 @@ export class Sala {
     
   id?: Number;
   numerosala?: Number;
+  nomesala: String;
   capacidade?: Number;
   bloco? : String;
   andar? : Number;
+  qtdcomputadores: Number;
 
 }
